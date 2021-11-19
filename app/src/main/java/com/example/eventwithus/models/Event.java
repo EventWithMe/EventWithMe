60a8fc1a3c809febc66e6ed6d440611003a93b21
Commando-Brando <branmuller@gmail.com>
2021-11-19 12:04:08 -0600

    added model classes and Event Stream RecyclerView

---
+++ app/src/main/java/com/example/eventwithus/models/Event.java
@@ -0,0 +1,90 @@
+package com.example.eventwithus.models;
+
+import java.time.LocalDate;
+
+public class Event {
+
+    private String eventName;
+    private String city;
+    private LocalDate date;
+    private String startTime; // TODO: 11/19/2021 figure out if there is a better way to store time like some kind of time formatting
+    private int occupancy;
+    private String description;
+    private String venueName;
+    private String eventType;
+
+    public Event(String eventName, String city, LocalDate date, String startTime, int occupancy, String description, String venueName, String eventType) {
+        this.eventName = eventName;
+        this.city = city;
+        this.date = date;
+        this.startTime = startTime;
+        this.occupancy = occupancy;
+        this.description = description;
+        this.venueName = venueName;
+        this.eventType = eventType;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVenueName() {
+        return venueName;
+    }
+
+    public void setVenueName(String venueName) {
+        this.venueName = venueName;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+}
