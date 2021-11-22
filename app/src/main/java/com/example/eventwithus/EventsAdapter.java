5f68ffbddf480beebef4c8f9cdd3dbac14c91254
Commando-Brando <branmuller@gmail.com>
2021-11-21 21:47:09 -0600

    Events adapter created and Stream Fragment based code added

---
+++ app/src/main/java/com/example/eventwithus/EventsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.eventwithus;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.eventwithus.models.Event;
+
+import java.util.List;
+
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Event> events;
+
+    public EventsAdapter(Context context, List<Event> posts) {
+        this.context = context;
+        this.events = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.event_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Event event = events.get(position);
+        holder.bind(event);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvUsername;
+        private TextView tvDescription;
+        private ImageView ivImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(Event event){
+        }
+    }
+
+}
--- app/src/main/java/com/example/eventwithus/fragments/StreamFragment.java
+++ app/src/main/java/com/example/eventwithus/fragments/StreamFragment.java
@@ -5,16 +5,27 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.eventwithus.EventsAdapter;
 import com.example.eventwithus.R;
+import com.example.eventwithus.models.Event;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class StreamFragment extends Fragment {
 
+    public static final String TAG = "StreamFragment";
+    private RecyclerView rvEvents;
+    protected EventsAdapter adapter;
+    protected List<Event> allEvents;
 
     public StreamFragment() {
         // Required empty public constructor
@@ -29,6 +40,17 @@ public class StreamFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvEvents = view.findViewById(R.id.rvStreamList);
+        allEvents = new ArrayList<>();
+        adapter = new EventsAdapter(getContext(), allEvents);
+        rvEvents.setAdapter(adapter);
+        rvEvents.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    protected void populateEvents() {
+        // TODO: 11/21/2021 method will get data from the ticketmaster API & store it in the events model
     }
 
 }
