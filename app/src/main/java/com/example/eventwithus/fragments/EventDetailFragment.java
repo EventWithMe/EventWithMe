a5eb64976a80ab4173ef720ee42df61a2e0e0463
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-11-22 20:55:30 -0600

    added EventDetailFragment model Need to modify EventsAdapter.

--- app/src/main/java/com/example/eventwithus/fragments/EventDetailFragment.java
+++ app/src/main/java/com/example/eventwithus/fragments/EventDetailFragment.java
@@ -1,4 +1,13 @@
 package com.example.eventwithus.fragments;
 
 public class EventDetailFragment {
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
 }
