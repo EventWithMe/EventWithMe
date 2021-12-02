2b3bb4fdb6b0dc0d6a03fab30327c1e878c44acb
Commando-Brando <branmuller@gmail.com>
2021-12-02 13:50:15 -0600

    profile scene complete except for pfp

--- app/src/main/java/com/example/eventwithus/EditProfileActivity.java
+++ app/src/main/java/com/example/eventwithus/EditProfileActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditProfile extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
