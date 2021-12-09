f9cf7aa8390d71f380328136d9effaf973e3082d
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-12-08 20:32:17 -0600

    Adjusted Maps . Working on grabbing all events and displaying markers for them based on last search

--- app/src/main/java/com/example/eventwithus/fragments/MapViewFragment.java
+++ app/src/main/java/com/example/eventwithus/fragments/MapViewFragment.java
@@ -1,16 +1,38 @@
 package com.example.eventwithus.fragments;
 
+import static android.content.Context.LOCATION_SERVICE;
+
+
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.eventwithus.GetLocation;
 import com.example.eventwithus.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -18,27 +40,53 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapViewFragment extends Fragment {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback{
 
     MapView mMapView;
     private GoogleMap googleMap;
-
+    private Marker marker;
+    Button btnButton;
+    LocationManager locationManager;
+    private LocationCallback mLocationCallback;
+    private FusedLocationProviderClient mFusedLocationClient;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.location_fragment, container, false);
-
+         FusedLocationProviderClient mFusedLocationClient;
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
-
+        btnButton = rootView.findViewById(R.id.btnCurrentCity);
         mMapView.onResume(); // needed to get the map to display immediately
+
 
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Log.d("mylog", "Not granted");
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    } else
+                        requestLocation();
+                } else
+                    requestLocation();
+            }
+        });
+
+
 
         mMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -65,11 +113,78 @@ public class MapViewFragment extends Fragment {
                 // For zooming automatically to the location of the marker
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
             }
         });
-
+        locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location mCurrentLocation = locationResult.getLastLocation();
+                LatLng myCoordinates = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                String cityName = getCityName(myCoordinates);
+                Toast.makeText(getContext(), cityName, Toast.LENGTH_SHORT).show();
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myCoordinates, 13.0f));
+                if (marker == null) {
+                    marker = googleMap.addMarker(new MarkerOptions().position(myCoordinates));
+                } else
+                    marker.setPosition(myCoordinates);
+            }
+        };
         return rootView;
     }
+    private String getCityName(LatLng myCoordinates) {
+        String myCity = "";
+        Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(myCoordinates.latitude, myCoordinates.longitude, 1);
+            String address = addresses.get(0).getAddressLine(0);
+            myCity = addresses.get(0).getLocality();
+            Log.d("mylog", "Complete Address: " + addresses.toString());
+            Log.d("mylog", "Address: " + address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return myCity;
+    }
+
+
+    @SuppressLint("WrongConstant")
+    private void requestLocation() {
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_MEDIUM);
+        criteria.setPowerRequirement(Criteria.POWER_MEDIUM);
+        String provider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        Log.d("mylog", "In Requesting Location");
+        if (location != null && (System.currentTimeMillis() - location.getTime()) <= 1000 * 2) {
+            LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+            String cityName = getCityName(myCoordinates);
+            Toast.makeText(getContext(), cityName, Toast.LENGTH_SHORT).show();
+        } else {
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setNumUpdates(1);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            Log.d("mylog", "Last location too old getting new location!");
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+            mFusedLocationClient.requestLocationUpdates(locationRequest,
+                    mLocationCallback, Looper.myLooper());
+        }
+    }
+
+
 
     @Override
     public void onResume() {
@@ -93,5 +208,10 @@ public class MapViewFragment extends Fragment {
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
